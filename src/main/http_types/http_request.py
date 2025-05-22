58a9e629fd2dd4f1eefb29cc5d206506299bc228
diegorhcp89 <diegoferreiradoamaral@gmail.com>
2025-05-22 13:01:09 -0300

    Implementando buscador de registros

--- src/main/http_types/http_request.py
+++ src/main/http_types/http_request.py
@@ -1,5 +1,10 @@
 class HttpRequest:
-    def __init__(self, body: dict = None, header: dict = None, params: dict = None) -> None:
+    def __init__(self,
+                 body: dict = None,
+                 header: dict = None, 
+                 path_params: dict = None, 
+                 query: dict = None) -> None:
         self.body = body
         self.headers = header
-        self.params = params
+        self.path_params = path_params
+        self.query = query
